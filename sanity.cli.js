6e3ccec4104a5f41f2fdb3171d74d664b0caf3f8
Chun <chunglh@bap.jp>
2023-11-13 14:13:32 +0700

    first commit

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'vxl6kh4j',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.js
@@ -0,0 +1,12 @@
+import testimonials from './testimonials'
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import workExperience from './workExperience'
+import skills from './skills'
+import works from './works'
+
+export const schemaTypes = [].concat([
+  works,testimonials, brands, about, skills, workExperience, experiences, contact
+])
